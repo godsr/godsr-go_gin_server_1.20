b1843ddddc49826db200aabfb7044a6b0b41f4b9
godsr <egoist3008@daum.net>
2024-04-04 17:23:18 +0900

    model change

--- models/Car.go
+++ models/Car.go
@@ -4,7 +4,6 @@ import "gorm.io/gorm"
 
 type Car struct {
 	gorm.Model
-	Id       int    `json:"ID" gorm:"primary_key"`
 	In_Date  string `json:"in_date"`
 	Out_Date string `json:"out_date"`
 	Type     int    `json:"type"`
